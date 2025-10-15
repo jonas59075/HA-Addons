1ae15d1fcfd749d3875504164223cbc4e25c44e9
RNI <rainer.niehues@empasoft.ch>
2025-10-15 23:03:23 +0200

    Add Preload Addon

--- preload/run.sh
+++ preload/run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/with-contenv sh
 echo "🔁 Preload-Init gestartet ..."
 
-if [ -f /config/preload.sh ]; then
-    chmod +x /config/preload.sh
-    /bin/sh /config/preload.sh
-else
-    echo "⚠️  /config/preload.sh nicht gefunden."
-fi
+TRIES=0
+while [ ! -f /config/preload.sh ] && [ $TRIES -lt 10 ]; do
+  echo "⏳ preload.sh noch nicht verfügbar, versuche erneut..."
+  sleep 3
+  TRIES=$((TRIES+1))
+done
 
-echo "✅ Preload abgeschlossen."
+if [ -f /config/preload.sh ]; then
+  chmod +x /config/preload.sh
+  /bin/sh /config/preload.sh
+  echo "✅ preload.sh erfolgreich ausgeführt."
+else
+  echo "❌ preload.sh nach 30 Sekunden immer noch nicht vorhanden!"
+fi
